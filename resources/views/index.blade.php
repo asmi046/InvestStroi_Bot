33ca99d2024cd6e3c100a6834e5bac4e1ba27e72
asmi046 <asmi046@gmail.com>
2023-02-13 18:10:26 +0300

    Оформление карточьки

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,11 +28,22 @@
             <custom-select :label="'Количество комнат'" :list="['1','2', '3']" ></custom-select>
         </form>
 
-        <div class="flex flex-wrap">
-            <div class="w-1/2 border border-solid border-cborder h-5 rounded-lg">
+        <div class="flex flex-wrap justify-between">
+            <div class="w-[48%] h-auto p-2 border border-solid border-cborder rounded-lg flex flex-col overflow-hidden">
+                <div class="w-full h-full">
+                    <img class="w-3/4 h-full mx-auto object-contain" src="{{asset('img/tmp-plan.jpg')}}" alt="">
+                </div>
+                <h2>3 комнаты, 79 м²</h2>
+                <p>ул. Косухина д. 33</p>
+                <p>7 этаж</p>
+                <p>Цена: <strong>5 450 0000 р</strong> </p>
+                <div class="w-full flex flex-wrap pt-2 mt-auto">
+                    <a class="bg-sred text-white text-center px-5 py-2 rounded-lg"  href="">Подробнее</a>
+                    <a class="w-8 h-8 my-auto ml-auto bg-lbg hover:bg-lbg_h" href=""></a>
+                </div>
             </div>
 
-            <div class="w-1/2 border border-solid border-cborder h-5 rounded-lg">
+            <div class="w-[48%] border border-solid border-cborder h-5 rounded-lg">
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,24 +10,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
+        backgroundImage: {
+            'lbg':'url(/public/img/icons/like.svg)',
+            'lbg_h':'url(/public/img/icons/like_hover.svg)',
+        },
+        colors: {
         'sred': '#C10010',
         'sblue': '#003C8C',
         'cborder': '#A0A0A0',
         'cfill': '#F2F2F2'
-      },
+        },
 
-      fontFamily: {
+        fontFamily: {
         sans: ['Roboto', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
         'header': ['"Exo 2"']
-      },
+        },
     },
   },
   plugins: [
     plugin(function({ addBase, theme }) {
       addBase({
-        'body': { backgroundColor: theme('white') },
+        'body': { backgroundColor: theme('white'), fontFamily:"'Roboto'" },
+        'h2': {fontWeight:'600', fontSize:"18px", padding:"1em 0", fontFamily:"'Exo 2'"},
+        'p': {fontSize:"14px"},
       })
     })
   ],
