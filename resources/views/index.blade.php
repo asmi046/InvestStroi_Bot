89b7d021455cc26792225b671dada527db685e13
asmi046 <asmi046@gmail.com>
2023-02-14 19:12:09 +0300

    Меню

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,43 +10,40 @@
 
 @section('content')
     <header class="relative flex border-gray-300 text-white text-2xl flex-col bg-sblue font-header bg-center bg-cover" style="background-image:url({{asset('img/hbg.webp')}})">
-    <div class="w-full h-full bg-black bg-opacity-50 absolute left-0 top-0 z-10"></div>
-    <div class="container mx-auto px-4 py-4 flex justify-between">
-      <div class="logo m-auto ml-0 flex z-20">
-        <p class="">Подбор <br/>квартир</p>
-      </div>
-
-      <a class="menu bg-white w-9 h-9 rounded-full my-auto bg-no-repeat bg-center z-20" style="background-image:url({{asset('img/logo-blue.svg')}})" href="#"></a>
-    </div>
-    <div class="hdecor w-full h-5 bg-white rounded-t-xl z-20"></div>
-  </header>
-
-  <main class="h-10" id="global_app">
-    <div class="container mx-auto px-4">
-        <form action="">
-            <custom-select :label="'Выберите дом'" :list="['Дом 1','Дом 3', 'Дом 2']" ></custom-select>
-            <custom-select :label="'Количество комнат'" :list="['1','2', '3']" ></custom-select>
-        </form>
-
-        <div class="flex flex-wrap justify-between">
-            <div class="w-[48%] h-auto p-2 border border-solid border-cborder rounded-lg flex flex-col overflow-hidden">
-                <div class="w-full h-full">
-                    <img class="w-3/4 h-full mx-auto object-contain" src="{{asset('img/tmp-plan.jpg')}}" alt="">
-                </div>
-                <h2>3 комнаты, 79 м²</h2>
-                <p>ул. Косухина д. 33</p>
-                <p>7 этаж</p>
-                <p>Цена: <strong>5 450 0000 р</strong> </p>
-                <div class="w-full flex flex-wrap pt-2 mt-auto">
-                    <a class="bg-sred text-white text-center px-5 py-2 rounded-lg"  href="">Подробнее</a>
-                    <a class="w-8 h-8 my-auto ml-auto bg-lbg hover:bg-lbg_h" href=""></a>
-                </div>
+        <div class="w-full h-full bg-black bg-opacity-50 absolute left-0 top-0 z-10"></div>
+        <div class="container mx-auto px-4 py-4 flex justify-between">
+            <div class="logo m-auto ml-0 flex z-20">
+                <p class="">Подбор <br/>квартир</p>
             </div>
 
-            <div class="w-[48%] border border-solid border-cborder h-5 rounded-lg">
+            <a class="menu bg-white w-9 h-9 rounded-full my-auto bg-no-repeat bg-center z-20" style="background-image:url({{asset('img/logo-blue.svg')}})" href="#"></a>
+        </div>
+
+        <div class="hdecor w-full h-5 bg-white rounded-t-xl z-20"></div>
+    </header>
+
+    <main class="h-10" id="global_app">
+        <div class="container mx-auto px-4">
+            <form action="">
+                <custom-select :label="'Выберите дом'" :list="['Дом 1','Дом 3', 'Дом 2']" ></custom-select>
+                <custom-select :label="'Количество комнат'" :list="['1','2', '3']" ></custom-select>
+            </form>
+
+            <div class="flex flex-wrap justify-between">
+                @for ($i =0; $i<12; $i++)
+                    <x-home-card></x-home-card>
+                @endfor
             </div>
         </div>
-    </div>
-
   </main>
+
+  <div class="fixed flex-wrap left-0 bottom-0 w-full h-16 bg-white borderShadow flex justify-between text-cborder px-1 py-2 xl:px-4">
+
+        <x-botom-menu-punkt :msg="'Поделиться'" :icon="'bg-shreBg'" :icon_h="'group-hover:bg-shreBg_h'"></x-botom-menu-punkt>
+        <x-botom-menu-punkt :msg="'Позвонить'" :icon="'bg-callBg'" :icon_h="'group-hover:bg-callBg_h'"></x-botom-menu-punkt>
+        <x-botom-menu-punkt :msg="'Написать'" :icon="'bg-msgBg'" :icon_h="'group-hover:bg-msgBg_h'"></x-botom-menu-punkt>
+        <x-botom-menu-punkt :msg="'Избраное'" :icon="'bg-likeBg'" :icon_h="'group-hover:bg-likeBg_h'"></x-botom-menu-punkt>
+        <x-botom-menu-punkt :msg="'Подбор'" :icon="'bg-searchBg'" :icon_h="'group-hover:bg-searchBg_h'"></x-botom-menu-punkt>
+
+  </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,31 @@ module.exports = {
         backgroundImage: {
             'lbg':'url(/public/img/icons/like.svg)',
             'lbg_h':'url(/public/img/icons/like_hover.svg)',
+
+            'shreBg': 'url(/public/img/icons/share.svg)',
+            'shreBg_h': 'url(/public/img/icons/share_hover.svg)',
+
+            'callBg': 'url(/public/img/icons/coll.svg)',
+            'callBg_h': 'url(/public/img/icons/coll_hover.svg)',
+
+            'msgBg': 'url(/public/img/icons/msg.svg)',
+            'msgBg_h': 'url(/public/img/icons/msg_hover.svg)',
+
+            'likeBg': 'url(/public/img/icons/like.svg)',
+            'likeBg_h': 'url(/public/img/icons/like_hover.svg)',
+
+            'searchBg': 'url(/public/img/icons/serch.svg)',
+            'searchBg_h': 'url(/public/img/icons/serch_hover.svg)',
         },
         colors: {
         'sred': '#C10010',
         'sblue': '#003C8C',
         'cborder': '#A0A0A0',
         'cfill': '#F2F2F2'
+        },
+
+        fontSize: {
+            min: ['10px', '14px'],
         },
 
         fontFamily: {
@@ -34,6 +53,8 @@ module.exports = {
         'body': { backgroundColor: theme('white'), fontFamily:"'Roboto'" },
         'h2': {fontWeight:'600', fontSize:"18px", padding:"1em 0", fontFamily:"'Exo 2'"},
         'p': {fontSize:"14px"},
+        '.borderShadow' : {boxShadow: "0px 0px 9px rgba(0, 0, 0, 0.25)"}
+
       })
     })
   ],
